1450330d5526229e69269ed6a7d6ce4dbc2ae51d
kon <sound.recording.man@gmail.com>
2016-05-31 17:22:32 +0900

    最初のコミット

---
+++ IntervalComposition/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using IntervalComposition.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntervalComposition
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel mainVM;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainVM = new MainViewModel();
+            this.DataContext = mainVM;
+        }
+
+        private void files_Drop(object sender, DragEventArgs e)
+        {
+            string[] fileNames = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            mainVM.AddImages(fileNames);
+        }
+    }
+}
---
+++ IntervalComposition/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace IntervalComposition.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 入力画像リスト
+        /// </summary>
+        public ObservableCollection<string> Images { get; set; }
+
+        private int width = 0;
+        private int height = 0;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainViewModel() 
+        {
+            Images = new ObservableCollection<string>();
+        }
+
+        /// <summary>
+        /// 画像を追加します
+        /// </summary>
+        /// <param name="pathes"></param>
+        public void AddImages(string[] pathes)
+        {
+            foreach (string path in pathes)
+            {
+                // 拡張子のチェック
+                string extention = System.IO.Path.GetExtension(path).ToUpper();
+                if (extention != ".JPEG" && extention != ".JPG") continue;
+
+                // サイズチェック
+                BitmapImage bImg = new BitmapImage(new Uri(path));
+                if (width == 0 || height == 0)
+                {
+                    width = bImg.PixelWidth;
+                    height = bImg.PixelHeight;
+                    Images.Add(path);
+                }
+                else
+                {
+                    if (width == bImg.PixelWidth && height == bImg.PixelHeight)
+                    {
+                        Images.Add(path);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
